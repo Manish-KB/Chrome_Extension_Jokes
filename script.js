5497fc241a38fa25421e15926221d9dfa9816556
Manish <you@example.com>
2023-06-19 16:00:45 +0530

    BUtton visibility during loading

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
+ myButton = document.getElementById("myButton");
+ myButton.addEventListener("click", changeJoke);
+
+
 
 fetch("https://icanhazdadjoke.com/slack").then(data =>
     data.json()).then(jokeData => {
         const jokeText = jokeData.attachments[0].text;
         const jokeElement = document.getElementById('jokeElement');
-        jokeElement.innerHTML = jokeText
+        jokeElement.innerHTML = jokeText;
+        myButton.style.visibility = "visible";
     });
 
-const myButton = document.getElementById("myButton");
-myButton.addEventListener("click", changeJoke);
+// const myButton = document.getElementById("myButton");
+
 
 function changeJoke() {
+    myButton.style.visibility="hidden";
+    innerh="<p id=\"jokeElement\"><img class=\"load\" src=\"loading.gif\" alt=\"loading\"></p>";
+    
+    jokeElement.innerHTML=innerh
     fetch("https://icanhazdadjoke.com/slack").then(data =>
         data.json()).then(jokeData => {
             const jokeText = jokeData.attachments[0].text;
             const jokeElement = document.getElementById('jokeElement');
-            jokeElement.innerHTML = jokeText
+            jokeElement.innerHTML = jokeText;
+            myButton.style.visibility = "visible";
         });
 }
